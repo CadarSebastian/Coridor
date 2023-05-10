737e99486acabfa5d6c3d35a20cb79465b4a6fa1
CadarSebastian <sebicadar03@gmail.com>
2023-05-10 22:40:57 +0300

    board facut kinda

---
+++ board.js
@@ -0,0 +1,55 @@
+function setup(){
+    createCanvas(800,800)
+}
+function draw(){
+
+    background("#00F6F0")
+    
+    c();
+
+    createMatrice();
+    
+}
+let x=30;
+let y=30;
+
+function createMatrice(){
+    let mat=[];
+    for(let i=0;i<=9;i++){
+        mat.push([]);
+        for (let j = 0; j < coloane; j++){
+            mat[i].push({
+                value:square(x,y,55,10),
+                color:("#FDC372")
+
+
+
+            })
+        }
+    }
+}
+
+function c(){
+    for(let i=0;i<=10;i++){
+        for(let j =0;j<=10;j++){
+            if(i==0 && j==0 || i==10 &&j==10 ||i==0 &&j==10 ||i==10 &&j==0){
+                
+            }else if (j==10){
+                rect(x,y,60,50,10)
+            }else if(i==0){
+                rect(x,y-10,50,60,10)
+            }else if(i==10){
+                rect(x,y,50,60,10)
+            }else if(j==0 ){
+                rect(x-10,y,60,50,10) 
+            }
+            else{
+            square(x,y,50,10)    
+            }
+        x=x+60;    
+        }
+        x=30;
+        y=y+60;
+    }
+    
+}
