78848110936e5ec873445ca4777fb6929f7c05bc
CadarSebastian <sebicadar03@gmail.com>
2023-05-11 16:07:50 +0300

    it finaly clicked (board)

--- board.js
+++ board.js
@@ -1,57 +1,66 @@
-function setup(){
-    createCanvas(720,700);
-    
+function setup() {
+    createCanvas(720, 700);
+    initBricks();
+
 }
-function draw(){
+function draw() {
     background("#00F6F0");
-    
-    c();
-    
-   
-}
-let x=30;
-let y=30;
+    showBricks();
+    showPion1();
 
 
-class spot{
-    #y;
-    #x;
-    color;
-    #width;
-    #height;
-    constructor(x,y,color){
-        this.#x=x;
-        this.#y=y;
-        this.color=color;
-        this.#height=height;
-        this.#width= width;
-    }
+
+
 }
 
-let table=[];
 
-function c(){
-    for(let i=0;i<=10;i++){
-        for(let j =0;j<=10;j++){
-            if(i==0 && j==0 || i==10 &&j==10 ||i==0 &&j==10 ||i==10 &&j==0){
-            
-            }else if (j==10){
-                rect(x,y,60,50,10)
-            }else if(i==0){
-                rect(x,y-10,50,60,10)
-            }else if(i==10){
-                rect(x,y,50,60,10)
-            }else if(j==0 ){
-                rect(x-10,y,60,50,10) 
-            }
-            else{
-            square(x,y,50,10)    
-            }
-        x=x+60;    
+
+let table = [];
+
+
+
+function initBricks() {
+    const bricksNumber = 5;
+    const marginLeft = 20;
+    const marginRigth = 20;
+
+    const bricksDistance = 10;
+
+    const brickWidth = 50;
+
+
+    for (let i = 0; i < 9; i++) {
+        let brick = [];
+        for (let j = 0; j < 9; j++) {
+            brick.push({
+                color: "pink",
+                x: marginLeft + j * bricksDistance + j * brickWidth,
+                y: bricky,
+                width: brickWidth,
+                heigth: 50,
+                margin: 10
+            });
         }
-        x=30;
-        y=y+60;
+        bricky = bricky + 60;
+        console.log(bricky);
+        bricks.push(brick);
     }
-    
+    console.log(bricks);
 }
 
+function showBricks() {
+    for (let i = 0; i < bricks.length; i++) {
+        for (let j = 0; j < bricks.length; j++) {
+            fill(bricks[i][j].color);
+            rect(bricks[i][j].x, bricks[i][j].y, bricks[i][j].width, bricks[i][j].heigth);
+        }
+
+
+    }
+}
+
+
+function getBrickX(){
+    
+
+}
---
+++ vars.js
@@ -0,0 +1,8 @@
+let i,j;
+let bricks=[];
+let bricky=40;
+
+let limitTop = 0;
+let limitBottom = 600;
+let limitLeft = 0;
+let limitRight = 600;
