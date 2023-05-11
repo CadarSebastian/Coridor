6cded8da3e8ceb10e090942e970626c861896f5c
CadarSebastian <sebicadar03@gmail.com>
2023-05-11 16:08:01 +0300

    start pion movement

--- pion1.js
+++ pion1.js
@@ -1,14 +1,21 @@
-function initPion1(){
-    let circleObj ={
-        x:100,
-        y:100,
-        size:40,
-        color:blue
 
-    }
+
+
+
+function initPion1(){
+    
     
 }
 
 function showPion1(){
+    let circleObj ={
+        x:bricks[0][4].x+25,
+        y:bricks[0][4].y+25,
+        size:40,
+        color:"blue"
+
+    }
+    fill(circleObj.color);
     circle(circleObj.x,circleObj.y,circleObj.size);
 }
+
