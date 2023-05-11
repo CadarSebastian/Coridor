c1f43bff4ec6d007ae1316331c04e7d1a0ab0663
CadarSebastian <sebicadar03@gmail.com>
2023-05-11 11:22:54 +0300

    nothings working

--- board.js
+++ board.js
@@ -1,25 +1,82 @@
 function setup(){
     createCanvas(720,700);
-    
+    createTable();
+    //initBricks();
 }
 function draw(){
-
     background("#00F6F0")
-    
-    c();
-    showPion1();
-    createMatrice();
-    
-    
+    showTable();
+    //showBricks();
+    const s= new square(30,30,50,50,color)
 }
 let x=30;
 let y=30;
+let bricks=[];
+
+class square{
+    #y;
+    #x;
+    color;
+    #width;
+    #height;
+    constructor(x,y,color){
+        this.#x=x;
+        this.#y=y;
+        this.color=color;
+        this.#height=height;
+        this.#width= width;
+    }
+}
+
+let table=[];
+
+function createTable(){
+    for(let i=0;i<=9;i++){
+        const s= new square(30,30,50,50,color)
+        
+    }
+}
+
+function showTable(){
+    for (let i = 0; i < 9; i++) {
+        fill(table[i].color);
+        rect(table[i].x, table[i].y, table[i].width, table[i].heigth);
+    }
+}
+
+
+
+function initBricks() {
+  
+
+    for (let i = 0; i < 9; i++) {
+        bricks.push({
+            color: "#0000ff",
+            x: 30,
+            y: 30,
+            width: 50,
+            heigth: 50,
+            
+        });
+    }
+}
+
+function showBricks() {
+    for (let i = 0; i < 9; i++) {
+        fill(bricks[i].color);
+        rect(bricks[i].x, bricks[i].y, bricks[i].width, bricks[i].heigth);
+    }
+}
+
+
+
+
 
 function createMatrice(){
     let mat=[];
     for(let i=0;i<=9;i++){
         mat.push([]);
-        for (let j = 0; j < coloane; j++){
+        for (let j = 0; j < 10; j++){
             mat[i].push({
                 value:square(x,y,55,10),
                 color:("#FDC372")
@@ -32,7 +89,7 @@ function c(){
     for(let i=0;i<=10;i++){
         for(let j =0;j<=10;j++){
             if(i==0 && j==0 || i==10 &&j==10 ||i==0 &&j==10 ||i==10 &&j==0){
-                
+            
             }else if (j==10){
                 rect(x,y,60,50,10)
             }else if(i==0){
