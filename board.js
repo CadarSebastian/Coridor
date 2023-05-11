435059cc088a91721c8389024b3f1b03bb134a72
CadarSebastian <sebicadar03@gmail.com>
2023-05-11 11:58:19 +0300

    scrap la incercari ca nu mere si nici ce inainte n

--- board.js
+++ board.js
@@ -1,19 +1,19 @@
 function setup(){
     createCanvas(720,700);
-    createTable();
-    //initBricks();
+    
 }
 function draw(){
-    background("#00F6F0")
-    showTable();
-    //showBricks();
-    const s= new square(30,30,50,50,color)
+    background("#00F6F0");
+    
+    c();
+    
+   
 }
 let x=30;
 let y=30;
-let bricks=[];
 
-class square{
+
+class spot{
     #y;
     #x;
     color;
@@ -29,61 +29,6 @@ class square{
 }
 
 let table=[];
-
-function createTable(){
-    for(let i=0;i<=9;i++){
-        const s= new square(30,30,50,50,color)
-        
-    }
-}
-
-function showTable(){
-    for (let i = 0; i < 9; i++) {
-        fill(table[i].color);
-        rect(table[i].x, table[i].y, table[i].width, table[i].heigth);
-    }
-}
-
-
-
-function initBricks() {
-  
-
-    for (let i = 0; i < 9; i++) {
-        bricks.push({
-            color: "#0000ff",
-            x: 30,
-            y: 30,
-            width: 50,
-            heigth: 50,
-            
-        });
-    }
-}
-
-function showBricks() {
-    for (let i = 0; i < 9; i++) {
-        fill(bricks[i].color);
-        rect(bricks[i].x, bricks[i].y, bricks[i].width, bricks[i].heigth);
-    }
-}
-
-
-
-
-
-function createMatrice(){
-    let mat=[];
-    for(let i=0;i<=9;i++){
-        mat.push([]);
-        for (let j = 0; j < 10; j++){
-            mat[i].push({
-                value:square(x,y,55,10),
-                color:("#FDC372")
-            })
-        }
-    }
-}
 
 function c(){
     for(let i=0;i<=10;i++){
@@ -109,3 +54,4 @@ function c(){
     }
     
 }
+
